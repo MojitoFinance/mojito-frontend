f1698d634f95776594c8e6b1b69e7b334947d384
bbelly <bawei3600@gmail.com>
2021-08-24 01:34:43 +0800

    update

--- i18n.js
+++ i18n.js
@@ -1,11 +1,11 @@
-const NextI18Next = require('next-i18next').default
-const { localeSubpaths } = require('next/config').default().publicRuntimeConfig
-const path = require('path')
+const NextI18Next = require('next-i18next').default;
+const { localeSubpaths } = require('next/config').default().publicRuntimeConfig;
+const path = require('path');
 
 module.exports = new NextI18Next({
   defaultLanguage: 'en',
   otherLanguages: ['cn'],
   localeSubpaths,
-  localePath: path.resolve('./locales'),
-  shallowRender: true
-})
+  localePath: path.resolve('./public/static/locales'),
+  shallowRender: true,
+});
